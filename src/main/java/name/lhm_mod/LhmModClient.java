3ba6bf5999d50bb1485648969b641f4464676102
wsnd0 <wsnd090909@163.com>
2025-01-09 07:45:57 +0800

    初始

---
+++ src/main/java/name/lhm_mod/LhmModClient.java
@@ -0,0 +1,10 @@
+package name.lhm_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LhmModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
